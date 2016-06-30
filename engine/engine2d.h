9ff00aa5336233e37de96f90f1a2305fcb6e2872
gbox3d <gbox3d@gmail.com>
2016-07-01 00:21:44 +0900

    ...

---
+++ engine/engine2d.h
@@ -0,0 +1,76 @@
+#ifndef __ENGINE_2D__
+#define __ENGINE_2D__
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <time.h>
+#include <sys/time.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/select.h>
+#include <termios.h>
+
+void reset_terminal_mode();
+int getch();
+int kbhit();
+void set_conio_terminal_mode();
+
+struct termios orig_termios;
+
+void reset_terminal_mode()
+{
+	tcsetattr(0, TCSANOW, &orig_termios);
+}
+
+void set_conio_terminal_mode()
+{
+	struct termios new_termios;
+
+	/* take two copies - one for now, one for later */
+	tcgetattr(0, &orig_termios);
+	memcpy(&new_termios, &orig_termios, sizeof(new_termios));
+
+	/* register cleanup handler, and set the new terminal mode */
+	atexit(reset_terminal_mode);
+	cfmakeraw(&new_termios);
+	tcsetattr(0, TCSANOW, &new_termios);
+}
+
+int kbhit()
+{
+	struct timeval tv = { 0L, 0L };
+	fd_set fds;
+	FD_ZERO(&fds);
+	FD_SET(0, &fds);
+	return select(1, &fds, NULL, NULL, &tv);
+}
+
+int getch()
+{
+	int r;
+	unsigned char c;
+	if ((r = read(0, &c, sizeof(c))) < 0) {
+		return r;
+	} else {
+		return c;
+	}
+}
+
+//-----------------------------------------------------
+//
+//
+
+void setColor(int fore_color,int backg_color)
+{
+	printf("%c[%d;%dm",0x1b,fore_color,backg_color);
+}
+
+void gotoxy(int x,int y)
+{
+	printf("%c[%d;%df",0x1b,y,x);
+
+}
+
+#endif
+
