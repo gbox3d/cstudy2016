a840e97f28fb61c66ef11a2263010b7448d0e9fb
gbox3d <gbox3d@gmail.com>
2016-07-06 17:47:07 +0900

    ...

--- day7/ex13.c
+++ day7/ex13.c
@@ -26,7 +26,50 @@ int main()
 			strLine[nTailIndex++] = pstr;
 		}
 		else if(!strcmp(pTemp,"pop")) {
-			
+			nTailIndex--;
+			free(strLine[nTailIndex]);
+			strLine[nTailIndex] = 0x00;
+		}
+		else if(!strcmp(pTemp,"del")) {
+			free(strLine[0]);
+			nTailIndex--;
+			for(int i=0;i<nTailIndex+1;i++) {
+				strLine[i] = strLine[i+1];
+			}
+		}
+		else if(!strcmp(pTemp,"ins")) {
+			//ins 2 hello
+			int nIndex = atoi(strtok(NULL," "));
+			pTemp = strtok(NULL,"");
+			printf("%d , %s 추가 \r\n",nIndex,pTemp);
+			int i;
+			for(i = nTailIndex;i > nIndex;i--) {
+				strLine[i] = strLine[i-1];
+			}
+			char *pstr = (char *)malloc(strlen(pTemp)+1);
+			strcpy(pstr,pTemp);
+			strLine[i] =  pstr;
+			nTailIndex++;
+
+		}
+		else if(!strcmp(pTemp,"rm")) {
+			//rm 1
+			int nIndex = atoi(strtok(NULL,""));
+			free(strLine[nIndex]);
+			for(int i = nIndex;i<nTailIndex;i++) {
+				strLine[i] = strLine[i+1];
+			}
+			nTailIndex--;
+			strLine[nTailIndex] = 0x00;
+		}
+		else if(!strcmp(pTemp,"set")) {
+			//set 1 new string
+			int nIndex = atoi(strtok(NULL," "));
+			free(strLine[nIndex]);
+			pTemp = strtok(NULL,"");
+			char *pstr = (char *)malloc(strlen(pTemp)+1);
+			strcpy(pstr,pTemp);
+			strLine[nIndex] = pstr;
 		}
 		else if(!strcmp(pTemp,"dump")) {
 			
