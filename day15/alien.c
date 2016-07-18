f7b02d7e1a6bd89aa261e8a0de92ae1ecc95bd21
gbox3d <gbox3d@gmail.com>
2016-07-18 14:11:25 +0900

    ex5

---
+++ day15/alien.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <sys/time.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/select.h>
+#include <termios.h>
+
+#include "../engine/engine2d.h"
+#include "../mapEditor/map.h"
+
+#include "alien.h"
+
+static void Apply(_S_ALIEN_OBJECT *pObj,double deltaTick)
+{
+	switch(pObj->m_nFSM) {
+
+		case 0: //die
+			break;
+			break;
+
+	}
+
+}
+static void Draw(_S_ALIEN_OBJECT *pObj, _S_MAP_OBJECT *pBuff)
+{
+	switch(pObj->m_nFSM) 
+	{
+		case 0:
+			break;
+		default :
+			map_drawTile_trn(pObj->m_pBody,
+					(int)(pObj->m_fXpos + pObj->m_fCenterX),
+					(int)(pObj->m_fYpos + pObj->m_fCenterY),
+					pBuff);
+
+			break;
+	}
+}
+
+void alien_init(_S_ALIEN_OBJECT *pObj,_S_MAP_OBJECT *pBody)
+{
+	pObj->m_nFSM = 0;
+	pObj->m_nStep = 0;
+	pObj->m_pBody = pBody;
+	pObj->m_fYpos = 0;
+	pObj->m_fXpos = 0;
+
+	pObj->m_fCenterX = 0 - (pBody->m_header.m_nWidth/2);
+	pObj->m_fCenterY = 0 - (pBody->m_header.m_nHeight/2);
+
+	pObj->pfApply = Apply;
+	pObj->pfDraw = Draw;
+
+
+}
+
+
---
+++ day15/ex5.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <sys/time.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/select.h>
+#include <termios.h>
+#include <math.h>
+
+#include "../engine/engine2d.h"
+#include "../mapEditor/map.h"
+
+#include "alien.h"
+
+struct timespec work_timer;
+double acc_tick,last_tick;
+int bLoop = 1;
+
+_S_MAP_OBJECT gScreenBuf[2];
+
+_S_MAP_OBJECT gAlienModel;
+
+_S_ALIEN_OBJECT gTestAlienObject;
+
+int main()
+{
+
+	for(int i=0;i<2;i++)
+	{
+		map_init(&gScreenBuf[i]);
+		map_new(&gScreenBuf[i],35,16);
+	}
+	
+	system("clear");
+	
+	set_conio_terminal_mode();
+	acc_tick=last_tick=0;
+
+	while(bLoop) {
+		//타이밍처리 
+		clock_gettime(CLOCK_MONOTONIC,&work_timer);
+		double cur_tick = work_timer.tv_sec + 
+			(double)(work_timer.tv_nsec * 1e-9);
+		double delta_tick = cur_tick - last_tick;
+		last_tick = cur_tick;
+		//실시간 입력
+		if(kbhit() != 0) {
+			char ch = getch();
+			if(ch == 'q') {
+				bLoop = 0;
+				puts("bye~ \r");
+			}
+
+		}
+		
+		//타이밍 계산 
+		acc_tick += delta_tick;
+		if(acc_tick > 0.1) {
+			gotoxy(0,0);
+			map_drawTile(&gScreenBuf[0],0,0,&gScreenBuf[1]);
+		
+			
+			map_dump(&gScreenBuf[1],Default_Tilepalete);
+			acc_tick = 0;
+		}
+
+	}
+
+	return 0;
+
+}
